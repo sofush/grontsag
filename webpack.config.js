96deb3bbb4ed22bfef5b290ef734b32d08acfff5
sofush <102423577+sofush@users.noreply.github.com>
2024-11-19 13:23:15 +0100

    chore: node and dependencies configuration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import process from 'process';
+import path from 'path';
+
+export default {
+  mode: 'none',
+  entry: {
+    about: './public/about.jsx',
+    index: './public/index.jsx',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    publicPath: '/dist/',
+    path: path.resolve(process.cwd(), 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/about.html',
+      filename: 'about.html',
+      chunks: ['about'],
+    }),
+  ],
+};
