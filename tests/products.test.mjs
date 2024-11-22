44f3324210c2f95562f4a47269c67917081817e0
sofush <102423577+sofush@users.noreply.github.com>
2024-11-22 23:24:07 +0100

    chore: fix indentation in tests

--- tests/products.test.mjs
+++ tests/products.test.mjs
@@ -44,87 +44,87 @@ const products = [
 ];
 
 beforeAll(async () => {
-  db = await MongoMemoryServer.create();
-  await mongoose.connect(db.getUri());
+    db = await MongoMemoryServer.create();
+    await mongoose.connect(db.getUri());
 
-  for (const idx in products) {
-    const product = new Product(products[idx]);
-    await product.save();
-  }
+    for (const idx in products) {
+        const product = new Product(products[idx]);
+        await product.save();
+    }
 
-  app = new Server(3000);
-  app.start();
+    app = new Server(3000);
+    app.start();
 });
 
 afterAll(async () => {
-  await mongoose.connection.close();
-  await db.stop();
-  app.close();
+    await mongoose.connection.close();
+    await db.stop();
+    app.close();
 });
 
 describe('GET /products', () => {
-  it('should be able to get all products', async () => {
-    const res = await request(app.server)
-      .get('/products')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200);
+    it('should be able to get all products', async () => {
+        const res = await request(app.server)
+            .get('/products')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200);
 
-    expect(res.body).toEqual(products);
-  });
+        expect(res.body).toEqual(products);
+    });
 
-  it('should be able to get a single products', async () => {
-    const res = await request(app.server)
-      .get('/products?idx=1&limit=1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200);
+    it('should be able to get a single products', async () => {
+        const res = await request(app.server)
+            .get('/products?idx=1&limit=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200);
 
-    expect(res.body).toEqual([ products[1] ]);
-  });
+        expect(res.body).toEqual([products[1]]);
+    });
 
-  it('should be able to read two products with a limit', async () => {
-    const res = await request(app.server)
-      .get('/products?idx=1&limit=2')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200);
+    it('should be able to read two products with a limit', async () => {
+        const res = await request(app.server)
+            .get('/products?idx=1&limit=2')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200);
 
-    expect(res.body).toEqual([ products[1], products[2] ]);
-  });
+        expect(res.body).toEqual([products[1], products[2]]);
+    });
 
-  it('should fail to read a product with invalid index', async () => {
-    return request(app.server)
-      .get('/products?idx=-1&limit=1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(400);
-  });
+    it('should fail to read a product with invalid index', async () => {
+        return request(app.server)
+            .get('/products?idx=-1&limit=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400);
+    });
 
-  it('should fail to read a product with invalid limit', async () => {
-    return request(app.server)
-      .get('/products?idx=1&limit=-1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(400);
-  });
+    it('should fail to read a product with invalid limit', async () => {
+        return request(app.server)
+            .get('/products?idx=1&limit=-1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400);
+    });
 
-  it('should read no products at out-of-bounds index', async () => {
-    const res = await request(app.server)
-      .get('/products?idx=99999999&limit=1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200);
+    it('should read no products at out-of-bounds index', async () => {
+        const res = await request(app.server)
+            .get('/products?idx=99999999&limit=1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200);
 
-    expect(res.body).toEqual([]);
-  });
+        expect(res.body).toEqual([]);
+    });
 });
 
 describe('GET /', () => {
-  it('should return HTML on front page', () => {
-    return request(app.server)
-      .get('/')
-      .expect('Content-Type', 'text/html; charset=UTF-8')
-      .expect(200);
-  });
+    it('should return HTML on front page', () => {
+        return request(app.server)
+            .get('/')
+            .expect('Content-Type', 'text/html; charset=UTF-8')
+            .expect(200);
+    });
 });
