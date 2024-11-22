4207cd75542b5937c5b3de024f9615f980d1bb1e
sofush <102423577+sofush@users.noreply.github.com>
2024-11-22 21:57:53 +0100

    refactor: improve productSchema

--- src/models/product.mjs
+++ src/models/product.mjs
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-    uuid: String,
-    name: String,
-    description: String,
-    price: Number,
-    unit: String,
-    image: String,
+    uuid: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    unit: { type: Number, required: true },
+    image: { type: Number, required: true },
 });
 
 export default mongoose.model('Product', productSchema);
