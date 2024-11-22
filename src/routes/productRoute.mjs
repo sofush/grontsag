d5310eeec3802163122adda8a1ba482cd898d704
sofush <102423577+sofush@users.noreply.github.com>
2024-11-22 22:43:27 +0100

    refactor: fix indentation

--- src/routes/productRoute.mjs
+++ src/routes/productRoute.mjs
@@ -5,28 +5,28 @@ import ProductController from '../controllers/productController.mjs';
 const router = express.Router();
 const productController = new ProductController();
 const requestValidator = Joi.object({
-	idx: Joi
-    .number()
-    .integer()
-    .min(0),
+    idx: Joi
+        .number()
+        .integer()
+        .min(0),
 
-	limit: Joi
-    .number()
-    .integer()
-    .min(1),
+    limit: Joi
+        .number()
+        .integer()
+        .min(1),
 });
 
 router.use(express.json());
 
 const validateQuery = (req, res, next) => {
-  const { error } = requestValidator.validate(req.query);
+    const { error } = requestValidator.validate(req.query);
 
-  if (error) {
-    console.error(error);
-    return res.status(400).json({ error: 'Input is invalid.' });
-  }
+    if (error) {
+        console.error(error);
+        return res.status(400).json({ error: 'Input is invalid.' });
+    }
 
-  next();
+    next();
 };
 
 router.get('/products', validateQuery, async (req, res) => {
