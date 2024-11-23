77fc524a9da5961141121defb1b2d31a22a7c82e
sofush <102423577+sofush@users.noreply.github.com>
2024-11-23 17:36:04 +0100

    chore: update tests for /login, /register and /api/user

--- tests/user.test.mjs
+++ tests/user.test.mjs
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import { validate as uuidValidate } from 'uuid';
 import Server from '../src/server.mjs';
 
+const jwtRegex = /^(?:[\w-]+\.){2}[\w-]+$/;
 let app;
 let db;
 
@@ -22,64 +23,85 @@ afterAll(async () => {
   app.close();
 });
 
-describe('GET /user with invalid, or no token', () => {
-    it('should return a new user when no JWT is present', async () => {
-        return request(app.server)
-            .get('/user')
+describe('GET /api/user/new', () => {
+    it('should return a new user when no jwt is present', async () => {
+        const res = await request(app.server)
+            .get('/api/user/new')
             .set('Accept', 'application/json')
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200);
 
-        // TODO: expect/validate content to be jwt
+        expect(res.body.jwt).toMatch(jwtRegex);
     });
 
-    it('should not succeed with an invalid token', () => {
-        return request(app.server)
-            .get('/user')
+    it('should return a new user when a jwt is present', async () => {
+        const res = await request(app.server)
+            .get('/api/user/new')
             .set('Accept', 'application/json')
-            .set('Authorization', 'not.a.valid.token')
+            .set('Authorization', 'should.ignore.this')
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(500);
+            .expect(200);
+
+        expect(res.body.jwt).toMatch(jwtRegex);
     });
 });
 
-describe('GET /user with valid token', () => {
+describe('GET /api/user with invalid, or no token', () => {
+    it('should not succeed with an invalid token', () => {
+        return request(app.server)
+            .get('/api/user')
+            .set('Accept', 'application/json')
+            .set('Authorization', 'not.a.valid.token')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(403);
+    });
+});
+
+describe('GET /api/user with valid token', () => {
     let jwt;
 
     beforeEach(async () => {
-        jwt = await request(app.server)
-            .get('/user')
+        const res = await request(app.server)
+            .get('/api/user/new')
             .set('Accept', 'application/json')
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200);
 
-        // TODO: expect/validate content to be jwt
+        expect(res.body.jwt).toMatch(jwtRegex);
+        jwt = res.body.jwt;
     });
 
     it('should return an existing user when valid token is present', async () => {
         const res = await request(app.server)
-            .get('/user')
+            .get('/api/user')
             .set('Accept', 'application/json')
             .set('Authorization', jwt)
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200);
 
-        expect(uuidValidate(res.uuid)).toBeTruthy();
+        expect(uuidValidate(res.body.id)).toBeTruthy();
+        expect(res.body.email).toBe(null);
+        expect(res.body.password).toBe(undefined);
     });
 
     it('should update the user with a valid token', async () => {
+        const email = 'email@example.org';
+        const password = 'verysecurepassword123';
         const res = await request(app.server)
-            .patch('/user')
+            .patch('/api/user')
             .set('Accept', 'application/json')
             .set('Authorization', jwt)
-            .send({
-                email: 'email@example.org',
-                password: 'verysecurepassword123',
-            })
+            .send({ email: email, password: password })
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200);
 
-        expect(uuidValidate(res.uuid)).toBeTruthy();
+        const jwtRet = res.body.jwt;
+        const userRet = res.body.user;
+
+        expect(uuidValidate(userRet.id)).toBeTruthy();
+        expect(userRet.email).toBe(email);
+        expect(userRet.password).toBe(undefined);
+        expect(jwtRet).toMatch(jwtRegex);
     });
 });
 
